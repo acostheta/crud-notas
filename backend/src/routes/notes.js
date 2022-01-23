b7666f6953c251270d11c94165356a32ccbbb5cb
acostheta <acostalaaj@gmail.com>
2022-01-23 15:25:38 -0400

    start creating routes, only notes routs for now

--- backend/src/routes/notes.js
+++ backend/src/routes/notes.js
@@ -2,7 +2,15 @@ const { Router } = require('express');
 const router = Router();
 
 
-// Hagamos que desde nuestro enrutador, escuchamos una ruta '/
-router.route('/').get((req, res) => res.send('Notes Routes'));
+// Hagamos que desde nuestro enrutador, escuchamos una ruta '/'
+router.route('/')
+    .get((req, res) => res.send('Notes Routes'))
+    .post();
+
+// Este tiene la diferencia de que se usa para peticiones a elementos específicos distinguidos por id.
+router.route('/:id')
+    .get()
+    .put()
+    .delete();
 
 module.exports = router;
