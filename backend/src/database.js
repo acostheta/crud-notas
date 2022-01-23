e2e190d8a73b8ce3a46b9164eefe5664166d99c6
acostheta <acostalaaj@gmail.com>
2022-01-23 10:49:45 -0400

    feat: added ternary in URI to connect DB on environtmen variable while available else use localhost/crudtest

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const URI = process.env.MONGODB_URI;
+const URI = process.env.MONGODB_URI 
+    ? process.env.MONGODB_URI
+    : 'mongodb://localhost/crudtest';
 
 mongoose.connect(URI), {
     newUrlParser: true,
