34a0696bdb7599e8220f6b6b61e37ea55df1e330
acostheta <acostalaaj@gmail.com>
2022-01-23 14:43:48 -0400

    feat: created 'port' variable for taking the value of the server port else use 4000

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,5 +4,31 @@
 const express = require('express');
 const app = express(); /* Todo el serv está en esta variable. */
 
+
+//  Settings
+
+/* Aqui se pueden poner varias cosas como:
+- Poner nombre al servidor
+- Un motor de plantillas
+- Puerto definido
+- Otras variables finas para el servidor */
+
+app.set('port', process.env.PORT || 4000); 
+
+
+
+// Middlewares
+
+
+
+
+
+
+// Routes
+
+
+
+
+
 module.exports = app;
 /* Aqui termina el inicio del servidor. Esto exporta app, que se toma en index.js y allí se corre. */
