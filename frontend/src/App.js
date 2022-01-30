3be2f1e86dbe6e207d99fc0cd30bd667944b4f0e
acostheta <acostalaaj@gmail.com>
2022-01-30 19:49:27 -0400

    components created and also routes.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
-
+import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import NotesList from './components/NotesList';
+import Navigation from './components/Navigation';
+import CreateNote from './components/CreateNote';
+import CreateUser from './components/CreateUser';
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+    <Navigation />
+        <Routes>
+          <Route path="/" element={<NotesList/>} />
+          <Route path="/edit/:id" element={<CreateNote/>} />
+          <Route path="/create" element={<CreateNote/>} />
+          <Route path="/user" element={<CreateUser/>} />
+        </Routes>
+    </Router>
   );
 }
 
