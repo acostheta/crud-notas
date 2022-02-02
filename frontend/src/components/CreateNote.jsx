7fff078271e16f08cdb9edbb5178c554d8169d27
acostheta <acostalaaj@gmail.com>
2022-02-02 19:22:01 -0400

    feature: tenemos un formulario para crear notas bien lindo.

--- frontend/src/components/CreateNote.jsx
+++ frontend/src/components/CreateNote.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
 
 export default function CreateNote() {
-  return <div>CreateNote</div>;
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card card-body">
+            <h3>Create Note </h3>
+            <hr/>
+            <div className="mb-3">
+              <label for="noteTitle" className="form-label">Title:</label>
+              <input type="text" className="form-control" id="formGroupExampleInput" placeholder="e.g: Workout"/>
+            </div>
+            <div className="mb-3">
+              <label for="noteContent" className="form-label">Content</label>
+              <textarea rows="2" className="form-control" id="formGroupExampleInput2" placeholder="e.g: I have to do some workout today at 6:30pm. "/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
